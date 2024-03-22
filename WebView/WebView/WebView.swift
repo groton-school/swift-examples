484809a78b131257d57048d3a717e6603906cc1f
Seth Battis <sbattis@groton.org>
2024-03-22 09:01:52 -0400

    Tidy up comments

--- WebView/WebView/WebView.swift
+++ WebView/WebView/WebView.swift
@@ -12,13 +12,16 @@ struct WebView: UIViewRepresentable {
 
     let webView = WKWebView()
     let url: URL
+            
+    func makeUIView(context: Context) -> some WKWebView {
+        // for testing
+        WKWebsiteDataStore.default().removeData(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes(), modifiedSince: Date(timeIntervalSince1970: 0), completionHandler: {})
         
-     func makeUIView(context: Context) -> some WKWebView {
-         let request = URLRequest(url: url)
-         webView.uiDelegate = context.coordinator
-         webView.navigationDelegate = context.coordinator
-         webView.load(request)
-         return webView
+        var request = URLRequest(url: url)
+        webView.uiDelegate = context.coordinator
+        webView.navigationDelegate = context.coordinator
+        webView.load(request)
+        return webView
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
@@ -40,25 +43,21 @@ struct WebView: UIViewRepresentable {
             self.parent = parent
         }
         
+        /**
+         * Examine navigation actions to redirect non-HTTP URL schemes to app
+         */
         func webView(_ webView: WKWebView,
                      decidePolicyFor navigationAction: WKNavigationAction,
                      decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
 
-            // if the request is a non-http(s) schema, then have the UIApplication handle
-            // opening the request
             if let url = navigationAction.request.url,
                 !url.absoluteString.hasPrefix("http://"),
                 !url.absoluteString.hasPrefix("https://"),
                 UIApplication.shared.canOpenURL(url) {
-
-                // have UIApplication handle the url (sms:, tel:, mailto:, ...)
                 UIApplication.shared.open(url, options: [:], completionHandler: nil)
-
-                // cancel the request (handled by UIApplication)
                 decisionHandler(.cancel)
             }
             else {
-                // allow the request
                 decisionHandler(.allow)
             }
         }
